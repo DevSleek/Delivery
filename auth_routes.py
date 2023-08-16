e0e77ecffcf4a6bc48224932a94b619226179137
SukhrobTuraev <suhrobturaev2004@gmail.com>
2023-08-16 14:22:50 +0500

    Delivery loyihasini yaratish va routerlani ulash

---
+++ auth_routes.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+auth_router = APIRouter(
+    prefix='/auth'
+)
+
+@auth_router.get('/')
+async def signup():
+    return {'message': 'This is auth rote signup page'}
