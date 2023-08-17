d4d361db1df2f960cffcc6d9a1daebd48c656d35
SukhrobTuraev <suhrobturaev2004@gmail.com>
2023-08-17 13:58:21 +0500

    Pydantic kutubxonasi bilan ishlash. User Creation

--- auth_routes.py
+++ auth_routes.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.exceptions import HTTPException
+
+from schemas import SignUpModel
+from database import session, engine
+from models import User
 
 auth_router = APIRouter(
     prefix='/auth'
@@ -7,3 +12,28 @@ auth_router = APIRouter(
 @auth_router.get('/')
 async def signup():
     return {'message': 'This is auth rote signup page'}
+
+@auth_router.post('/signup')
+async def signup(user: SignUpModel):
+    db_email = session.query(User).filter(User.email == user.email).first()
+    if db_email is not None:
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='User with this email already exists'
+        )
+
+    db_username = session.query(User).filter(User.username == user.username).first()
+    if db_username is not None:
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='User with this username already exists'
+        )
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+        is_staff=user.is_staff,
+        is_active=user.is_active
+    )
+
